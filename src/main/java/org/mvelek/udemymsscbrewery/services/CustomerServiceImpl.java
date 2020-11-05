ec3cc85241138e1a7c35998c9e3b3a2477ad97e5
mvelek <mvelek@gmail.com>
2020-11-05 21:57:24 +0100

    - add REST methods for Customer

--- src/main/java/org/mvelek/udemymsscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/org/mvelek/udemymsscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package org.mvelek.udemymsscbrewery.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mvelek.udemymsscbrewery.web.model.CustomerDto;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -13,5 +15,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("mike")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl - would add a real impl to update customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
     }
 }
